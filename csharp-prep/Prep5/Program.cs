07aa870918db282167bba6467393763b23438db5
Miguel <miguel.coelho1599@gmail.com>
2023-11-04 23:09:48 -0700

    Completed 5th, final assignment.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        
+        string userName = UserName();
+        int userNumber = FavNum();
+        int squaredNumber = SquareNumber(userNumber);
+       
+        DisplayFinal(userName, squaredNumber);
+
+        static void DisplayMessage()
+            {
+                Console.WriteLine("Hello world!");
+            }
+        static string UserName()
+            {
+                Console.WriteLine("What is your name? ");
+                string name = Console.ReadLine();
+                
+                return name;
+            }
+        static int FavNum()
+            {
+                Console.WriteLine("What is your favorite number? ");
+                int number = int.Parse(Console.ReadLine());
+                
+                return number;
+            }
+        static int SquareNumber(int number)
+            {
+                int square = (int)Math.Sqrt(number);
+                
+                return square;
+            }
+        static void DisplayFinal(string name, int square)
+            {
+                Console.WriteLine($"{name}, the square of your number is {square}.");
+            }
+        
     }
 }
