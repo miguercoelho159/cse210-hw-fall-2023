9aec5d6390cd383c502b9de5b047c8e07dbed4d8
Miguel <miguel.coelho1599@gmail.com>
2023-11-04 22:43:27 -0700

    Completed 4th assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int inputNumber = -1; 
+    
+        while (inputNumber != 0)
+        { 
+            Console.WriteLine("Enter a number (0 to exit): ");
+            
+            string userResponse = Console.ReadLine();
+            inputNumber = int.Parse(userResponse);
+            
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        
+        }
+
+        int total = numbers.Sum();
+        Console.WriteLine($"The sum is: {total}");
+
+        int average = (int)numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+    
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 }
