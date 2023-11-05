18f5c967a86147649bbe45c9a8dfd5286fa883e2
Miguel <miguel.coelho1599@gmail.com>
2023-11-04 21:34:34 -0700

    Completed 2nd and 3rd assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int grade_percentage = int.Parse(answer);
+
+        string letter = "";
+         
+        if (grade_percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade_percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade_percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+         
+        Console.WriteLine($"Your grade is: {letter}");
+
+
+        if ( grade_percentage >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, better luck next time!");
+        }
+
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,101);
+
+        int guess = 1;
+
+        while (guess != magic_number)
+        {        
+
+            Console.WriteLine("What is your guess? ");
+            string string_guess = Console.ReadLine();
+            guess = int.Parse(string_guess);
+
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Lower!");
+            }
+
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher!");
+            }
+
+            else 
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
+        
+
+
+
     }
 }
